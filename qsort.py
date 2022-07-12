0552db5fa89fcaf2f22eae8ac346c97e1b06cf10
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-07-12 15:19:19 +0200

    Add files via upload
    Full quick sort implementation

---
+++ qsort.py
@@ -0,0 +1,119 @@
+import random
+from random import randint
+
+
+def inputs():
+    """
+    Fonction qui demande à l'utilisateur quelle est 
+    la taille de la liste qu'il veut entrer, ainsi que les membres
+    de la liste
+    
+    Inputs :
+        -a[e]
+        -l
+    Return :
+        -a
+        -l
+    """
+    a = []
+    l = int(input("Taille de la liste = "))
+    for e in range(0, l):
+        e = int(input("élément à ajouter : "))
+        a.append(e)
+    print("fin d'entrée de données")
+    return a, l
+
+
+def rand():
+    """
+    Fonction qui demande à l'utilisateur la taille de 
+    la liste qu'il veut générer.
+    
+    Inputs : 
+        -n 
+    Return : 
+        -a
+    """
+    
+    n = int(input("Entrez la taille de la liste\n"))
+    a = [i for i in range(1, n + 1)]
+    random.shuffle(a)
+    print("Liste non triée : {0}\n" .format(a))
+    return a
+
+
+def splitlist(a, low, high):
+    """
+    Effectue une section de la liste "a" par la droite (pivot = membre 
+    de la liste le plus haut)
+
+    Args:
+        -a 
+        -low
+        -high
+
+    Returns:
+        -index + 1 
+    """
+    pivot = a[high]
+    i = low-1
+    
+    for j in range(low, high):
+        if a[j]<=pivot:
+            i=i+1
+
+            (a[i], a[j]) = (a[j], a[i])   
+
+    (a[i + 1], a[high]) = (a[high], a[i + 1])
+    
+    return i+1
+    
+
+def qsort(a, low, high):
+    """
+    Effectue un Quic Sort sur la liste a
+
+    Args:
+        -a
+        -low
+        -high 
+
+    Returns:
+        -a (sorted)
+    """
+    if low < high:
+        pi = splitlist(a, low, high)
+        qsort(a, low, pi-1)
+        qsort(a, pi+1, high)
+        return a
+    
+        
+
+    
+
+def main():
+    """
+    Demande à l'utilisateur s'il veut rentrer une liste à la 
+    main ou si il veut en générer un dans un ordre aléatoire.
+    
+    Ensuite la fonction exécute un qsort sur cette même liste.
+    """
+    print("  ____   _____  ____  _____ _______ \n / __ \ / ____|/ __ \|  __ \__   __|\n| |  | | (___ | |  | | |__) | | |\n| |  | |\___ \| |  | |  _  /  | |\n| |__| |____) | |__| | | \ \  | |\n \___\_\_____/ \____/|_|  \_\ |_|\n")
+    ans = input("Vous voulez entrer des nombres (1) ou les générer aléatoirement (2) ?")
+    if ans == '1':
+        a, l = inputs()
+    elif ans =='2':
+        a = rand()
+    else :
+        print("Entrez une valeure valide (1 ou 2).")
+        exit(0)
+    low = a[0]
+    high = a[-1]
+    qsort(a, 0, len(a) - 1)
+    print(f"Liste triée : {a}")
+    
+    
+    
+if __name__ =="__main__":
+    main()
+    
